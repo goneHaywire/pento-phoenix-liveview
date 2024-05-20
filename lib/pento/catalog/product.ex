183e0b261f56aaef438f84e4bb50f5edad11c39e
Emiljan Dusha <emiljanthegreat@gmail.com>
2024-05-20 23:36:39 +0200

    feat: ch3 & ch4

---
+++ lib/pento/catalog/product.ex
@@ -0,0 +1,38 @@
+defmodule Pento.Catalog.Product do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "products" do
+    field :description, :string
+    field :name, :string
+    field :sku, :integer
+    field :unit_price, :float
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(product, attrs) do
+    product
+    |> cast(attrs, [:name, :description, :unit_price, :sku])
+    |> validate_required([:name, :description, :unit_price, :sku])
+    |> unique_constraint(:sku)
+  end
+
+  def discount_changeset(product, discounted_price) do
+    product
+    |> cast(%{unit_price: discounted_price}, [:unit_price])
+    |> validate_required([:unit_price])
+    |> validate_discount_price()
+  end
+
+  def validate_discount_price(changeset) do
+    current_price = changeset.data().unit_price
+
+    changeset
+    |> validate_number(:unit_price,
+      less_than: current_price,
+      message: "discounted price must be less than current price"
+    )
+  end
+end
